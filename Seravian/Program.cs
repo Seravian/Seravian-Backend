75d13761d12141d700b768255183a562fdf88da3
Mohamed Saeed <mohamed.saeed.goda.13@gmail.com>
2025-04-25 07:19:07 +0300

    Move OpenAPI endpoint registration outside dev block

--- Seravian/Program.cs
+++ Seravian/Program.cs
@@ -26,11 +26,11 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+app.MapOpenApi();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-
     app.UseSwaggerUI(options =>
     {
         options.SwaggerEndpoint("/openapi/v1.json", "OpenAPI V1");
