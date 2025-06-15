293980fadc45487eb7c5d71bfdfda78f7f83da85
Mohamed Saeed <mohamed.saeed.goda.13@gmail.com>
2025-06-15 04:16:43 +0300

    Refactor FAQ endpoints to load questions from JSON file
    Implements a more maintainable approach for handling FAQ questions and answers by:
    - Moving hardcoded Q&A data to an external JSON file
    - Adding new endpoint for FAQ-specific questions
    - Updating project configuration to include JSON file in build output
    
    This change improves maintainability and makes it easier to update Q&A content without code changes.

--- Seravian/Controllers/PatientController.cs
+++ Seravian/Controllers/PatientController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Concurrent;
 using System.Net.Mail;
 using System.Security.Claims;
+using System.Text.Json;
 using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,91 +13,30 @@ using Seravian.DTOs.Doctor;
 [Authorize(Roles = "Patient")]
 public class PatientController : ControllerBase
 {
+    [HttpGet("faq-questions-answers")]
+    public ActionResult<List<QuestionAnswerResponseDto>> FaqQuestionsAnswers()
+    {
+        var faqQuestionsAnswersJsonPath =
+            "QuestionsAndAnswers/realistic_mental_health_100_questions.json";
+
+        return Ok(
+            JsonSerializer.Deserialize<List<QuestionAnswerResponseDto>>(
+                System.IO.File.ReadAllText(faqQuestionsAnswersJsonPath)
+            )
+        );
+    }
+
     [HttpGet("questions-answers")]
     public ActionResult<List<QuestionAnswerResponseDto>> QuestionsAnswers()
     {
-        List<QuestionAnswerResponseDto> response =
-        [
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 1,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 2,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 3,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 4,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 5,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 6,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 7,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 8,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 9,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 10,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 11,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 12,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-            new QuestionAnswerResponseDto
-            {
-                QuestionId = 13,
-                Question = "lorem ims afdamkv mkldmam l;mdsa",
-                Answer = "hoeap das;mas lorem",
-            },
-        ];
-        return Ok(response);
+        var QuestionsAnswersJsonPath =
+            "QuestionsAndAnswers/realistic_mental_health_100_questions.json";
+
+        return Ok(
+            JsonSerializer.Deserialize<List<QuestionAnswerResponseDto>>(
+                System.IO.File.ReadAllText(QuestionsAnswersJsonPath)
+            )
+        );
     }
 }
 
